77cea8c7b6b228cb9e9adec966e0214bc65ef1e3
neworderofjamie <neworderofjamie@gmail.com>
2022-12-02 10:55:38 +0000

    Revert "wrong training output commited"
    This reverts commit 1967664037eddf73da6eff8084d477fc281da6b1.

--- scripts/results/copy_matching_train.sh
+++ scripts/results/copy_matching_train.sh
@@ -2,5 +2,5 @@
 
 for TEST in test_output_0_512_100_dvs_gesture_1234_*.csv;
 do
-    cp ../../classifier_model/train_output_99${TEST#test_output_0} .;
+    cp ../../classifier_model/train${TEST#test} .;
 done
