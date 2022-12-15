93a7a6162f6c5cb393c46a1c0de22c635b027b31
neworderofjamie <neworderofjamie@gmail.com>
2022-12-15 13:03:32 +0000

    * update performance figure to include FPTT comparison * final paragraph of intro text * start on performance section

--- scripts/plot_performance.py
+++ scripts/plot_performance.py
@@ -30,11 +30,21 @@ train_fps = (TRAIN_EXAMPLES * TRAIN_STEPS) / train_data["Epoch train time [s]"]
 gpu_test = test_data[test_data["Backend"] == "GPU"]
 cpu_test = test_data[test_data["Backend"] == "CPU"]
 
+gpu_test_fps = test_fps.loc[gpu_test.index].values
+cpu_test_fps = test_fps.loc[cpu_test.index].values
+
+fptt_fps = TRAIN_EXAMPLES / 0.4
+print(f"Training speedup vs FPTT {train_fps.values[-1] / fptt_fps}")
+print(f"Sparse ALIF512R CPU inference speedup {cpu_test_fps[2] / cpu_test_fps[0]}")
+print(f"Sparse ALIF256F256 CPU inference speedup {cpu_test_fps[3] / cpu_test_fps[1]}")
+print(f"Sparse ALIF512R CPU training speedup {train_fps.values[2] / train_fps.values[0]}")
+print(f"Sparse ALIF256F256 CPU training speedup {train_fps.values[3] / train_fps.values[1]}")
 # Get configurations
 # **HACK** cos I cba to write a sorting function
-#configurations = np.intersect1d(test_data["Config"].unique(),
+#cconfigurations = np.intersect1d(test_data["Config"].unique(),
 #                                train_data["Config"].unique())
-configurations = ["ALIF512R", "ALIF256F256R", "ALIF512R sparse", "ALIF256F256R sparse"]
+configurations = ["512R", "256F256R", "512R sparse",
+                  "256F256R sparse", "1024F512R"]
 
 fig, axis = plt.subplots(figsize=(plot_settings.column_width, 2.0))
 
@@ -42,13 +52,14 @@ pal = sns.color_palette()
 bar_x = np.arange(len(configurations)) * GROUP_PAD
 
 # Show bars for train and test accuracy
-train = axis.bar(bar_x, train_fps.values,
+train = axis.bar(bar_x[:len(train_fps.values)], train_fps.values,
          width=BAR_WIDTH, color=pal[0])
-inference_gpu = axis.bar(bar_x + BAR_PAD, test_fps.loc[gpu_test.index].values,
+inference_gpu = axis.bar(bar_x[:len(gpu_test_fps)] + BAR_PAD, gpu_test_fps,
          width=BAR_WIDTH, color=pal[1])
-inference_cpu = axis.bar(bar_x + (2 * BAR_PAD), test_fps.loc[cpu_test.index].values,
+inference_cpu = axis.bar(bar_x[:len(cpu_test_fps)] + (2 * BAR_PAD), cpu_test_fps,
          width=BAR_WIDTH, color=pal[2])
-
+fptt = axis.bar([bar_x[-1] + BAR_PAD], [fptt_fps], 
+                width=BAR_WIDTH, color=pal[3])
 axis.axhline(1000.0, linestyle="--", color="gray")
 
 # Remove axis junk
@@ -59,10 +70,10 @@ axis.set_ylabel("Timesteps per second")
 axis.set_xticks(bar_x + BAR_PAD)
 axis.set_xticklabels([c.replace(" ", "\n") for c in configurations])
 
-fig.legend([train, inference_gpu, inference_cpu],
-           ["Train", "GPU inference", "CPU inference"],
-           loc="lower center", ncol=3, frameon=False)
-fig.tight_layout(pad=0, rect=[0.0, 0.125, 1.0, 1.0])
+fig.legend([train, fptt, inference_gpu, inference_cpu],
+           ["GeNN Train", "FPTT train", "GeNN GPU inference", "GeNN CPU inference"],
+           loc="lower center", ncol=2, frameon=False)
+fig.tight_layout(pad=0, rect=[0.0, 0.2, 1.0, 1.0])
 
 if not plot_settings.presentation and not plot_settings.poster:
     fig.savefig("../figures/performance.pdf")
